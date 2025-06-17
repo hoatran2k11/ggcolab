96b4d859d3dc89f65a4fb1ae0260832b55f0596a
Hòa Trần <HUYT27871@GMAIL.COM>
2025-06-17 19:03:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 # Cài QEMU và công cụ liên quan
-sudo apt-get update && apt-get install qemu -y
-sudo apt install qemu-utils -y
-sudo apt install qemu-system-x86-xen -y
-sudo apt install qemu-system-x86 -y
+sudo apt-get update && sudo apt-get install qemu qemu-utils qemu-system-x86-xen qemu-system-x86 -y
 
-# Tạo ổ cứng ảo 32GB định dạng RAW
-qemu-img create -f raw win.img 32G
+# Tạo ổ cứng ảo 32GB định dạng RAW nếu chưa tồn tại
+if [ ! -f win.img ]; then
+  qemu-img create -f raw win.img 32G
+fi
 
-# Tải file ISO Windows tùy chỉnh + ISO driver VirtIO
-wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O win.iso 'https://archive.org/download/en_windows_server_2022x64_dvd_/en_windows_server_2022x64_dvd_.iso'
+# Kiểm tra và tải file virtio-win.iso nếu chưa có
+if [ ! -f virtio-win.iso ]; then
+  wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
+else
+  echo "File virtio-win.iso đã tồn tại, bỏ qua tải."
+fi
+
+# Kiểm tra và tải file win.iso nếu chưa có
+if [ ! -f win.iso ]; then
+  wget -O win.iso 'https://archive.org/download/en_windows_server_2022x64_dvd_/en_windows_server_2022x64_dvd_.iso'
+else
+  echo "File win.iso đã tồn tại, bỏ qua tải."
+fi
 
 # Chạy máy ảo Windows qua QEMU
 sudo qemu-system-x86_64 \
@@ -22,5 +31,4 @@ sudo qemu-system-x86_64 \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -smp cores=2 \
-  -enable-kvm
+  -smp cores=2
